7d329c9059b2a4016a4ebf9c4eff649f689b1f08
SaraSorlin <sorlin82@gmail.com>
2023-12-01 08:30:33 +0100

    .

--- logIn.js
+++ logIn.js
@@ -14,6 +14,8 @@ const users = [
     number: 112
   }
 ]
+
+
 function ValidLogin(username, password) {
   for (let i = 0; i < users.length; i++) {
     if (users[i].username === username & users[i].password === password) {
@@ -22,8 +24,11 @@ function ValidLogin(username, password) {
   }
   return false;
 }
+
+
 let username = "admin"
 let password = 4321
+
 
 if (ValidLogin(username, password)) {
   console.log("Welcome", username)
