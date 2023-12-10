13d72061082db85678f7720827b02c1c99d77815
SpaceDaze01 <luunaastar@gmail.com>
2023-12-10 15:15:09 +0000

    update
    update

---
+++ filter.js
@@ -0,0 +1,18 @@
+//ska kunna filtrera valen av önskad bostad
+
+let filter = {
+  type: [],
+  price: [],
+  rooms: [],
+  area: [],
+  Balcony: [],
+  Outdoors: [],
+  Floor: [],
+  Elevator: [],
+  YearOfBuilding: [],
+  Storehouse: [],
+  ParkingLot: [],
+  Courtyard: []
+}
+
+
